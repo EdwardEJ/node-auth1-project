8e844fb11ae7abc77ebe51a705781dbf7c5e6a03
A <jesus.e.estrada@hotmail.com>
2020-10-12 16:31:14 -0500

    created migrations

---
+++ data/knex.js
@@ -0,0 +1,6 @@
+const knex = require('knex');
+
+const knexfile = require('../knexfile');
+const environment = process.env.ENVIRONMENT || 'development';
+
+module.exports = knex(knexfile[environment]);
